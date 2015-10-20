9d55ac1a8a377b99a52e4e4387ecb49581a9336a
kerimyildirim <yildirimker@itu.edu.tr>
2015-10-20 10:09:41 +0300

    made some arrangments

---
+++ players.py
@@ -0,0 +1,5 @@
+class Player:
+    def __init__(self, firstname, lastname, birthday=None):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.birthday = birthday
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import os
 
 from flask import Flask
 from flask import render_template
+from players import Player
 
 app = Flask(__name__)
 
@@ -13,7 +14,9 @@ def home():
     return render_template('home.html', current_time=now.ctime())
 @app.route('/players')
 def players():
-	return render_template('players.html')
+    player1 = {'name':'kerim'}
+    player2 = {'name':'ahmet'}
+    return render_template('players.html', player1=player1, player2=player2)
 
 
 
