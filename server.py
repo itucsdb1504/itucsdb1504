63cda9bb881976d937e347f83978ce90d121ce4d
ANIL <ANIL@ANIL-Bilgisayar>
2015-11-02 19:56:04 +0200

    Credentials for ElephantSQL added.

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import datetime
-import os
 import psycopg2
+import json
+import os
+import re
 
 import dbmanager
 
@@ -8,6 +10,19 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+
+
+def get_elephantsql_dsn(vcap_services):
+    """Returns the data source name for ElephantSQL."""
+    parsed = json.loads(vcap_services)
+    uri = parsed["elephantsql"][0]["credentials"]["uri"]
+    match = re.match('postgres://(.*?):(.*?)@(.*?)(:(\d+))?/(.*)', uri)
+    user, password, host, _, port, dbname = match.groups()
+    dsn = """user='{}' password='{}' host='{}' port={}
+             dbname='{}'""".format(user, password, host, port, dbname)
+    return dsn
+
+
 
 @app.route('/')
 def home():
@@ -91,4 +106,12 @@ if __name__ == '__main__':
         port, debug = int(VCAP_APP_PORT), False
     else:
         port, debug = 5000, True
+
+    VCAP_SERVICES = os.getenv('VCAP_SERVICES')
+    if VCAP_SERVICES is not None:
+        app.config['dsn'] = get_elephantsql_dsn(VCAP_SERVICES)
+    else:
+        app.config['dsn'] = """user='vagrant' password='vagrant'
+                               host='localhost' port=54321 dbname='itucsdb'"""
+
     app.run(host='0.0.0.0', port=port, debug=debug)
